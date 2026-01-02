c291cff3c196b7228a3c26a829f36965ae2e3efb
theniswara <daniswara.mdr@proton.me>
2026-01-02 07:56:15 +0700

    feat: Create subscribe_benefits table with fields for name, and subscribe package association

--- database/migrations/2026_01_01_153351_create_subscribe_benefits_table.php
+++ database/migrations/2026_01_01_153351_create_subscribe_benefits_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('subscribe_benefits', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('subscribe_package_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
