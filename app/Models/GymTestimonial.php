30ea912847fde53269440cf1f844ee035541c183
theniswara <daniswara.mdr@proton.me>
2026-01-03 17:06:16 +0700

    feat: Implement soft deletes and fillable field in GymTestimonial model

--- app/Models/GymTestimonial.php
+++ app/Models/GymTestimonial.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GymTestimonial extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'occupation',
+        'name',
+        'photo',
+        'message',
+        'gym_id',
+    ];
 }
