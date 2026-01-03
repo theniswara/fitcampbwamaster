11b7b1b500b9e8175f3ba87dfeefe4e959aaf783
theniswara <daniswara.mdr@proton.me>
2026-01-03 16:25:35 +0700

    feat: Implement soft deletes and fillable fields in City model

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class City extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'photo',
+    ];
 }
