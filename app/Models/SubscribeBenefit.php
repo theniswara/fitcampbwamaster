99f1d511f195713fb9a93338da11fb123f1e6bbf
theniswara <daniswara.mdr@proton.me>
2026-01-03 17:10:20 +0700

    feat: Implement soft deletes and fillable fields in SubscribeBenefit model

--- app/Models/SubscribeBenefit.php
+++ app/Models/SubscribeBenefit.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SubscribeBenefit extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'subscribe_package_id',
+    ];
 }
