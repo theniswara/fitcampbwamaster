3c5562859738bc4889939ff329e279316056953f
theniswara <daniswara.mdr@proton.me>
2026-01-03 17:02:51 +0700

    feat: Implement soft deletes and fillable field in GymPhoto model

--- app/Models/GymPhoto.php
+++ app/Models/GymPhoto.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GymPhoto extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'photo',
+        'gym_id',
+    ];
 }
