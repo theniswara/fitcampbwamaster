10ad2bd2776019d46e714e9bddb46220abdfd1e6
theniswara <daniswara.mdr@proton.me>
2026-01-03 16:52:50 +0700

    feat: Implement sof deletes and fillable in Gym & Facility table

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Facility extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'about',
+        'name',
+        'thumbnail',
+        'is_open',
+    ];
 }
--- app/Models/Gym.php
+++ app/Models/Gym.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Gym extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'thumbnail',
+        'name',
+        'slug',
+        'address',
+        'about',
+        'is_popular',
+        'open_time_at',
+        'closed_time_at',
+        'city_id',
+    ];
 }
