a6d03fba8b68e70c6f9280678a1897276a079a9e
theniswara <daniswara.mdr@proton.me>
2026-01-03 16:01:05 +0700

    feat: Implement soft deletes and fillable fields in SubscribeTransaction model

--- app/Models/SubscribeTransaction.php
+++ app/Models/SubscribeTransaction.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SubscribeTransaction extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'booking_trx_id',
+        'name',
+        'phone',
+        'email',
+        'proof',
+        'total_amount',
+        'duration',
+        'is_paid',
+        'started_at',
+        'ended_at',
+        'subcribe_package_id',
+    ];
 }
