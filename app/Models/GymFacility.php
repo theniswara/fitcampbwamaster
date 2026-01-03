e14cd43b501829e9da6fbeaf633be62153dc4d2d
theniswara <daniswara.mdr@proton.me>
2026-01-03 16:58:12 +0700

    feat: Implement soft deleets and fillable in GymFacility model

--- app/Models/GymFacility.php
+++ app/Models/GymFacility.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class GymFacility extends Model
 {
-    //
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'gym_id',
+        'facility_id',
+    ];
 }
--- database/migrations/2026_01_01_153117_create_gym_facilities_table.php
+++ database/migrations/2026_01_01_153117_create_gym_facilities_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('gym_facilities', function (Blueprint $table) {
-            $table->id();  
+            $table->id();
             $table->foreignId('gym_id')->constrained()->cascadeOnDelete();
             $table->foreignId('facility_id')->constrained()->cascadeOnDelete();
             $table->softDeletes();
