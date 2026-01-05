e06c6de328c7315b952dfa5af95c004c7900b5f3
theniswara <daniswara.mdr@proton.me>
2026-01-05 11:10:26 +0700

    feat: add name and photo columns to CitiesTable.

--- app/Filament/Resources/Cities/Tables/CitiesTable.php
+++ app/Filament/Resources/Cities/Tables/CitiesTable.php
@@ -7,6 +7,8 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -17,6 +19,10 @@ class CitiesTable
         return $table
             ->columns([
                 //
+                TextColumn::make('name')
+                    ->searchable(),
+
+                ImageColumn::make('photo'),
             ])
             ->filters([
                 TrashedFilter::make(),
