26f4972cf17dcf5c3035c2ec605d11189be6bcda
theniswara <daniswara.mdr@proton.me>
2026-01-06 20:41:08 +0700

    feat: Add name text input, thumbnail file upload fields, about text area, and is_open select options to the facility form schema.

--- app/Filament/Resources/Facilities/Schemas/FacilityForm.php
+++ app/Filament/Resources/Facilities/Schemas/FacilityForm.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\Facilities\Schemas;
 
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
 
 class FacilityForm
@@ -11,6 +15,23 @@ class FacilityForm
         return $schema
             ->components([
                 //
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                FileUpload::make('thumbnail')
+                    ->image()
+                    ->required(),
+
+                Textarea::make('about')
+                    ->required(),
+
+                Select::make('is_open')
+                    ->options([
+                        true => 'Open',
+                        false => 'Maintenance',
+                    ])
+                    ->required(),
             ]);
     }
 }
