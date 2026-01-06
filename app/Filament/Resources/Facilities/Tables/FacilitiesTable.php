7f84367bf89601b75162ee71a70e24912c92e58f
theniswara <daniswara.mdr@proton.me>
2026-01-06 20:42:36 +0700

    feat: Add name and thumbnail columns to FacilitiesTable.

--- app/Filament/Resources/Facilities/Tables/FacilitiesTable.php
+++ app/Filament/Resources/Facilities/Tables/FacilitiesTable.php
@@ -7,6 +7,8 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -17,6 +19,10 @@ class FacilitiesTable
         return $table
             ->columns([
                 //
+                TextColumn::make('name')
+                    ->searchable(),
+
+                ImageColumn::make('thumbnail'),
             ])
             ->filters([
                 TrashedFilter::make(),
