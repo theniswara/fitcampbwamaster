6889d13611cb70b6b49e2d7e000d83e43d6d309b
theniswara <daniswara.mdr@proton.me>
2026-01-07 12:37:19 +0700

    feat: Add photo, name, and gym name columns to the Gym Testimonials table.

--- app/Filament/Resources/GymTestimonials/Tables/GymTestimonialsTable.php
+++ app/Filament/Resources/GymTestimonials/Tables/GymTestimonialsTable.php
@@ -7,6 +7,8 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -17,6 +19,12 @@ class GymTestimonialsTable
         return $table
             ->columns([
                 //
+                ImageColumn::make('photo'),
+
+                TextColumn::make('name')
+                    ->searchable(),
+
+                TextColumn::make('gym.name'),
             ])
             ->filters([
                 TrashedFilter::make(),
