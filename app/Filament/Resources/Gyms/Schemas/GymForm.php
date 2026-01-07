42743078ebb66bd40eae92ae66615e57a97c05e2
theniswara <daniswara.mdr@proton.me>
2026-01-07 10:11:29 +0700

    feat: implement comprehensive form schema for gym details, photos, facilities, and operating hours.

--- app/Filament/Resources/Gyms/Schemas/GymForm.php
+++ app/Filament/Resources/Gyms/Schemas/GymForm.php
@@ -2,6 +2,14 @@
 
 namespace App\Filament\Resources\Gyms\Schemas;
 
+use App\Models\Facility;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TimePicker;
+use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Schema;
 
 class GymForm
@@ -11,6 +19,68 @@ class GymForm
         return $schema
             ->components([
                 //
+
+                Fieldset::make('Details')
+                    ->columns(1)
+                    ->schema([
+
+                        TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+
+                        Textarea::make('address')
+                            ->rows(3)
+                            ->required()
+                            ->maxLength(255),
+
+                        FileUpload::make('thumbnail')
+                            ->image()
+                            ->required(),
+
+                        Repeater::make('gymPhotos')
+                            ->relationship('gymPhotos')
+                            ->schema([
+                                FileUpload::make('photo')
+                                ->required(),
+                            ]),
+                    ]),
+
+                Fieldset::make('Additional')
+                ->columns(1)
+                ->schema([
+
+                    Textarea::make('about')
+                        ->required(),
+
+                    Repeater::make('gymFacilities')
+                        ->relationship('gymFacilities')
+                        ->schema([
+                            Select::make('facility_id')
+                                ->label('Gym Facility')
+                                ->options(Facility::all()->pluck('name', 'id'))
+                                ->searchable()
+                            ->required(),
+                        ]),
+
+                    Select::make('is_popular')
+                        ->options([
+                            true => 'Popular',
+                            false => 'Not Popular',
+                        ])
+                        ->required(),
+
+                    Select::make('city_id')
+                        ->relationship('city', 'name')
+                        ->searchable()
+                        ->preload()
+                        ->required(),
+
+                    TimePicker::make('open_time_at')
+                        ->required(),
+
+                    TimePicker::make('closed_time_at')
+                        ->required(),
+                ]),
             ]);
     }
 }
