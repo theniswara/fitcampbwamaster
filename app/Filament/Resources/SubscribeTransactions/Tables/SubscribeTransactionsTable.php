abfe1505f96509e8df8a484365a40f4de702202d
theniswara <daniswara.mdr@proton.me>
2026-01-19 13:15:41 +0700

    feat: Enhance subscribe transactions table with detailed columns, a package filter, a view action, and an action to approve payments.

--- app/Filament/Resources/SubscribeTransactions/Tables/SubscribeTransactionsTable.php
+++ app/Filament/Resources/SubscribeTransactions/Tables/SubscribeTransactionsTable.php
@@ -2,11 +2,19 @@
 
 namespace App\Filament\Resources\SubscribeTransactions\Tables;
 
+use App\Models\SubscribeTransaction;
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
 use Filament\Actions\RestoreBulkAction;
+use Filament\Actions\ViewAction;
+use Filament\Notifications\Notification;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
 
@@ -17,12 +25,50 @@ class SubscribeTransactionsTable
         return $table
             ->columns([
                 //
+                ImageColumn::make('subscribePackage.icon'),
+
+                TextColumn::make('name'),
+
+                TextColumn::make('booking_trx_id'),
+
+                IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Terverifikasi'),
+
             ])
             ->filters([
                 TrashedFilter::make(),
+
+                SelectFilter::make('subscribe_package_id')
+                    ->label('Subscribe Package')
+                ->relationship('subscribePackage', 'name'),
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
+
+                Action::make('approve')
+                    ->label('Approve')
+                    ->action(function (SubscribeTransaction $record) {
+                        $record->is_paid = true;
+                        $record->save();
+
+                        // Trigger the custom notificaton
+                        Notification::make()
+                            ->title('Transaction Approved')
+                            ->success()
+                            ->body('The transaction has been succesfully approved.')
+                            ->send();
+                    })
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(fn(SubscribeTransaction $record) => !$record->is_paid),
+
+
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
